b8a3ecdc526fca64b5d85cbb14efdc69616a276b
edi <edilsonclaudinosilva@gmail.com>
2021-08-10 23:29:01 -0300

    Passagen de Dados para segunda tela

--- app/src/main/java/com/graoscafe/view/TiposGrainsCafe.java
+++ app/src/main/java/com/graoscafe/view/TiposGrainsCafe.java
@@ -2,15 +2,43 @@ package com.graoscafe.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.graoscafe.R;
+import com.graoscafe.model.TiposGraosCafe;
+import com.graoscafe.services.SevicesGrains;
+
+import java.util.List;
 
 public class TiposGrainsCafe extends AppCompatActivity {
+    private final Gui gui = new Gui();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipos_grao_cafe);
+        Intent i = getIntent();
+        gui.ctx = this;
+        gui.tiposGraosCafeList = (List<TiposGraosCafe>) i.getSerializableExtra("typesCoffers");
+
+
     }
+
+
+
+
+    public  static class Gui{
+        private List<TiposGraosCafe> tiposGraosCafeList;
+        private Context ctx;
+
+    }
+
+
+
+
+
 }
