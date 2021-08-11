cc459779fbc3124d3228a2a9f9463c6b98915140
edi <edilsonclaudinosilva@gmail.com>
2021-08-10 21:49:15 -0300

    criação da tela de tipos de grão

--- app/src/main/java/com/graoscafe/view/TiposGraoCafe.java
+++ app/src/main/java/com/graoscafe/view/TiposGraoCafe.java
@@ -1,14 +1,16 @@
-package com.graoscafe;
+package com.graoscafe.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.graoscafe.R;
+
+public class TiposGraoCafe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tipos_grao_cafe);
     }
 }
